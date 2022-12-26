6f431c487af5765e8d5a146a2b399e2c0eca12aa
iskeletorr <ahmetalperen.7@gmail.com>
2022-12-26 13:11:29 +0300

    example added

---
+++ example/example.dart
@@ -0,0 +1,46 @@
+import 'package:flip_drawer_3d/flip_drawer_3d.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FlipDrawer3D(
+          appBar: (context, controller, controller2) => CustomAppBar(
+                color: Colors.pink,
+                horizontalController: controller,
+                verticalController: controller2,
+                title: Text('AppBar Text'),
+              ),
+          mainPage: (context, controller, controller2, controller3) => CustomNormalView(
+                child: Column(
+                  children: [
+                    Container(color: Colors.green, height: 200, child: Center(child: Text('Text'))),
+                    Expanded(
+                        child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(index.toString()),
+                      ),
+                    ))
+                  ],
+                ),
+              ),
+          contentPage: (context, controller) => Container(
+                color: Colors.pink,
+              ),
+          drawer: CustomDrawer(
+            backgroundColor: Colors.blue,
+            child: Column(),
+          )),
+    );
+  }
+}
